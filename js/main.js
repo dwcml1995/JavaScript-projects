2446d9e77a6c61c3d4657eb0375cf7460645aa39
Donovan Lundrigan <donavon892@gmail.com>
2024-08-27 12:53:53 -0300

    first commit for javascript projects

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var a = "hello"; // printing hello as an alert
+window.alert(a);
+
+var B = "Hey", C = "how are ya", D = "bedsy"; // assigned multiple variables, printing hey
+document.write(B);
+
+var sent1 = "This is the beginning of a string"; //conca
+var sent2 = " and this is the end of the string";
+var i = sent1 + sent2;
+document.write(i); 
+
+var x = 8; // writing and printing the result of my expression 
+var y = 14;
+var result = (x * y);
+document.write(result);
